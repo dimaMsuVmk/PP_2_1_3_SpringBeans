0bef241f08aefd98b1f2099968d2cc2255396611
dimaVmkMSU <mega.dima.r@mail.ru>
2023-06-25 18:42:39 +0300

    task_2.1.4

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,16 @@
 package app.config;
 
+import app.model.Animal;
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean("buldog")
+    public Animal geyAnimal() {
+        return new Dog();
+    }
 }
